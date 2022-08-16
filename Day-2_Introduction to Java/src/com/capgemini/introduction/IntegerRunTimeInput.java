ace166890ce82d618449e51cf58c82433a1cd98d
hp <hp@Yogesh>
2022-08-17 00:11:32 +0530

    Second commit

---
+++ Day-2_Introduction to Java/src/com/capgemini/introduction/IntegerRunTimeInput.java
@@ -0,0 +1,19 @@
+package com.capgemini.introduction;
+//program on integer=runtime input
+import java.util.Scanner;
+
+public class IntegerRunTimeInput {
+
+	public static void main(String[] args) {
+		
+		//variable initialization
+		int a=145;
+		System.out.println(a);
+		//"println" is used to print the output in the next line
+		//"print" is used to print the output in the same line
+		System.out.println("Hey Guys!");
+		System.out.println("Hey folks!");
+
+	}
+
+}
---
+++ Day-2_Introduction to Java/src/com/capgemini/introduction/StringProgram.java
@@ -0,0 +1,15 @@
+package com.capgemini.introduction;
+
+import java.util.Scanner;
+
+public class StringProgram {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		// String str=sc.next();
+		String str=sc.nextLine();
+		System.out.println(str);
+
+	}
+
+}
