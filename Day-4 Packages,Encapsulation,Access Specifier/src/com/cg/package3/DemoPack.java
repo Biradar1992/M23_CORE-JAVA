3a6cd8fa2205bd38b113eead8aa7d86915a9c2dc
hp <hp@Yogesh>
2022-08-25 13:20:09 +0530

    First commit

---
+++ Day-4 Packages,Encapsulation,Access Specifier/src/com/cg/package3/DemoPack.java
@@ -0,0 +1,27 @@
+package com.cg.package3;
+
+public class DemoPack
+{
+	public void accept()
+	{
+		System.out.println("WELCOME TO JAVA FULL STACK PROGRAM");
+	}
+
+}package com.cg.package3;
+
+public class HDFC 
+{
+	private int amount;
+	//getter method to return a value
+	public int getAmount() {
+		return amount;
+	}
+	//setter method to set a value
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	
+	
+
+}
