812b51c214ed8d20fa97cacb7ad00267b9bef184
hp <hp@Yogesh>
2022-08-30 09:49:08 +0530

    First Commit

---
+++ Day-12_Exception Handling/src/com/cg/exceptionhandling/ArithmaticException.java
@@ -0,0 +1,13 @@
+package com.cg.exceptionhandling;
+//program for byte date-type
+public class ArithmaticException {
+
+	public static void main(String[] args) {
+	    byte a=127;
+	    //byte a=129
+	    //it will give you error because of byte range
+		System.out.println(a);
+
+	}
+
+}
