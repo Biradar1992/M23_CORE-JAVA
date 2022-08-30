698c9a7d446aaab47ad51aa2ac34db06ef312809
hp <hp@Yogesh>
2022-08-30 09:25:34 +0530

    First Commit

---
+++ Day-11_String/src/com/tns/String/Example3.java
@@ -0,0 +1,16 @@
+package com.tns.String;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		        //String str="";
+				//String str1="Hariom";
+				String str2=new String("Yogesh");
+				String str3=new String("Yogesh");
+				//System.out.println(str3==str2);
+				System.out.println(str3.equals(str2));
+
+
+	}
+
+}
