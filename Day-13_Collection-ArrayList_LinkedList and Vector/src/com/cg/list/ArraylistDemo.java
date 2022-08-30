93e635d41cba945907125bf5a2e83832c40c2b95
hp <hp@Yogesh>
2022-08-30 11:56:42 +0530

    First Commit

---
+++ Day-13_Collection-ArrayList_LinkedList and Vector/src/com/cg/list/ArraylistDemo.java
@@ -0,0 +1,22 @@
+package com.cg.list;
+
+public class ArraylistDemo {
+
+}
+package com.cg.list;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArraylistDemo {
+
+	public static void main(String[] args) {
+		List<Integer>obj=new ArrayList<Integer>();
+		obj.add(12);
+		obj.add(23);
+		System.out.println(obj);
+
+	}
+
+}
---
+++ Day-13_Collection-ArrayList_LinkedList and Vector/src/com/cg/list/VectorExample.java
@@ -0,0 +1,16 @@
+package com.cg.list;
+
+import java.util.Vector;
+
+public class VectorExample {
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void main(String[] args) {
+		Vector v=new Vector();
+		v.add(11);
+		v.add("Yogesh");
+		System.out.println(v);
+
+	}
+
+}
